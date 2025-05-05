cf18fad36828b73c4ba7f71e0acd6f65f05d51d4
Intel Corporation <parker.lischwe@intel.com>
2025-05-05 06:47:43 -0700

    rag UI improvement

--- use-cases/langchain/video-summarization/summarizer/rag.py
+++ use-cases/langchain/video-summarization/summarizer/rag.py
@@ -7,39 +7,13 @@ def search_in_milvus(query_txt):
     with ThreadPoolExecutor() as pool:
         try:
             response = requests.get(url=MILVUS_SIM_SEARCH_ENDPOINT, params={"query": query_txt})
-            # print(response.content)
             if response.status_code != 200:
                 err_msg = {"error": f"Error in search_in_milvus function {response.status_code}: {response.content.decode('utf-8')}"}
                 print("Error message inside search func:", err_msg)
                 return err_msg
-                #return None
-            #return response.content
+
             return {"result": response.content.decode("utf-8")}
 
         except requests.exceptions.RequestException as e:
             print(f"Search in Milvus: Request failed: {e}")
             return {"error": str(e)}
-
-#if __name__ == "__main__":
-#    import argparse
-#    import ast
-#    from concurrent.futures import ThreadPoolExecutor
-
-#    parser = argparse.ArgumentParser() 
-#    parser.add_argument("--query_text", type=str)
-#    args = parser.parse_args()
-    
-#    with ThreadPoolExecutor() as pool:
-#        if args.query_text:
-#            print(f"Search Query: {args.query_text}")
-#            query_text = args.query_text
-#            query_future = pool.submit(search_in_milvus, query_text)
-#            result = query_future.result()
-#            if result and "result" in result:
-#                print(f"Search results: {ast.literal_eval(result['result'])}")
-
-            #if query_future and query_future.result():
-            #    print(f"Search Results: {ast.literal_eval(query_future.result().decode('utf-8'))}")
-        
-        #else:
-        #    print("No query text provided. Please provide a query text to search.")
--- use-cases/langchain/video-summarization/summarizer/streamlit_rag.py
+++ use-cases/langchain/video-summarization/summarizer/streamlit_rag.py
@@ -2,11 +2,20 @@ import streamlit as st
 import ast
 from rag import search_in_milvus
 
+def play_video(video_path, offset):
+    video_file = open(video_path, 'rb') 
+    video_bytes = video_file.read() 
+    st.video(video_bytes, start_time=int(offset))
+
 st.set_page_config(page_title="RAG Seach UI", layout="wide")
 
 st.title("Video RAG Search interface")
 
 query = st.text_input("Enter your query:", "")
+
+video_file = 'one-by-one-person-detection.mp4'
+
+col1, col2 = st.columns([1, 1])
 
 if st.button("Search"):
     with st.spinner("Searching..."):
@@ -19,6 +28,22 @@ if st.button("Search"):
         try:
             results = ast.literal_eval(response["result"])
             st.success("Results recieved!")
-            st.write(results)
+            chunk_summary = results["results"][0]["chunk_summary"]
+            chunk_path = results["results"][0]["chunk_path"]
+            start_time = results["results"][0]["start_time"]
+
+            with col2:
+                st.markdown("Textual Summary")
+                st.text_area(f"Summary of this segment: {chunk_summary}", height=300)
+            with col1:
+                if video_file:
+                    st.markdown("Video playback")
+                    play_video(video_file, start_time)
+
+            #print(f"\n\nSTART TIME: {start_time}\n\n")
+            #st.write(results)
+            #start_time = results["results"][0]["start_time"]
+            #print(f"\n\nStart time offset: {start_time}\n\n")
+
         except Exception as e:
             st.error(f"Failed to parse response: {e}")
